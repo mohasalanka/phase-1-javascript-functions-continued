f700bc88833f4b6a3621f46aa937bb87ed47169b
Mohamed <mohasalanka2020@gmail.com>
2023-03-14 18:56:29 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`
+}
+saturdayFun();
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+}
+mondayWork();
+
+function wrapAdjective(adjective = '*'){
+
+    return function(message){
+        // message = 'a hard worker'
+        return `You are ${adjective}${message}${adjective}!`
+    };
+}
+
+wrapAdjective('*')('a hard worker')('*')
+wrapAdjective('||')('a dedicated programmer')('||')
